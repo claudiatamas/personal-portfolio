0928e685d0b282b4c348342be424e464bf6c7455
claudiatamas <claudiatamas28@yahoo.com>
2024-11-27 22:11:21 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import Focuskeeper from './pages/Focuskeeper';
 import Work from './components/Work';
 import About from './components/About';
-import Therapyi from './pages/Therapyi';
+
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/work" element={<Work />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/focuskeeper" element={<Focuskeeper />} />
-        <Route path="/therapyi" element={<Therapyi />} />
+    
       </Routes>
     </Router>
   );
